5eb4560fb0a675473f499702246889899a27e138
TaeBeom Shin <50625090+TaeBeomShin@users.noreply.github.com>
2020-08-06 20:48:32 +0900

    [동적계획법] 11052
    생각이 조금 필요한 dp

--- 11052.cpp
+++ 11052.cpp
@@ -5,21 +5,25 @@ using namespace std;
 
 int main(){
 //D[N]=D[N-i]+P[i];
-//D[N]=max(D[N-i]+P[i]) 1<=i<=N -> N*O(N^2)
+//D[N]=min(D[N-i]+P[i]) 1<=i<=N -> N*O(N^2)
 	fastio
 	int N;
 	cin>>N;
 	int D[N+1]={0,};
 	int P[N+1]={0,};
 	
-	for(int i=1;i<=N;i++) cin>>P[i];
-	
+	for(int i=1;i<=N;i++){
+		cin>>P[i];
+		D[i]=P[i];
+	}
+	//최솟값을 구하기위해, D[i]를 미리 정의해놓는게 중요했음. 
 	for(int i=1;i<=N;i++){
 		for(int j=1;j<=i;j++){
-			D[i]=max(D[i],D[i-j]+P[j]);
+			D[i]=min(D[i],D[i-j]+P[j]);
 		}
 	}
 	
-	cout<<D[N];
+	cout<<D[N]<<"\n";
+	
 	return 0;
 }
