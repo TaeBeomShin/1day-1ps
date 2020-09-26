e594aaa444352bdea03f8c598194c76aee4824c5
taebeom <qja9605@gmail.com>
2020-09-26 22:54:32 +0900

    수정

--- 16953b.cpp
+++ 16953b.cpp
@@ -1,31 +1,27 @@
 #include<bits/stdc++.h>
+#define X first;
+#define Y second;
 #define MAX 1000000000
 using namespace std;
 
-int a,b;
-int dist[MAX];
 int main(){
-	cin>>a>>b;
-	//a->b로 바꾸는데 필요한 연사의 최솟값.
+	int a,b;cin>>a>>b;
+	bool finish= false;
 	
-	queue <int> Q;
-	Q.push(a);
-	dist[a]=1;
+	queue<pair<int,int> > Q;
+	Q.push({a,1});
 	
 	while(!Q.empty()){
-		int cur=Q.front(); Q.pop();
-		if(cur==b) break;
-		int arr[2]={cur*2,cur*10+1};
+		int num=Q.front().X;int count=Q.front().Y;
+		Q.pop();
 		
-		for(int i=0;i<2;i++){
-			int next=arr[i];
-			if(next>MAX) continue;
-			if(dist[next]) continue;
-			Q.push(next);
-			dist[next]=dist[cur]+1;
+		if(num==b){
+			cout<<count;
+			finish=true;
+			break;
 		}
+		if(num*2<=b) Q.push({num*2,count+1});
+		if(num<=MAX/10&&num*10+1<=b) Q.push({num*10+1,count+1});
 	}
-	cout<<dist[b];
-	
-	return 0;
+	if(!finish) cout<<-1; 
 }
