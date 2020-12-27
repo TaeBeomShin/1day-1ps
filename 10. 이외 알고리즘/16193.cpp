4383916bb0a10aa4ba3b0ea9b15abf1efb9fe1a6
taebeom <qja9605@gmail.com>
2020-12-28 02:30:18 +0900

    Update 16193.cpp

--- 10. 이외 알고리즘/16193.cpp
+++ 10. 이외 알고리즘/16193.cpp
@@ -1,52 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n;
+long long getSum(vector<int> &v){
+	long long sum=0;
+	for(int i=0;i<n-1;i++)
+		sum+=abs(v[i]-v[i+1]);
+	return sum;
+}
 int main(){
-	int n;cin>>n;
+	cin>>n;
 	vector<int> v(n),MAX,MIN;
 	for(int i=0;i<n;i++) cin>>v[i];
 	sort(v.begin(),v.end());
-	
-	long long result,sum;
-	int count;
+
 	if(n%2==0){
 		MAX.push_back(v[n/2-1]);
-		for(int i=0;i<n-1;i++){
-			MAX.push_back(v[n-i-1]);
-			MAX.push_back(v[i]);
-		}
-		sum=0;
-		for(int i=0;i<n-1;i++)
-			sum+=abs(MAX[i]-MAX[i+1]);
-		result=sum;
 		MIN.push_back(v[n/2-1]);
-		for(int i=0;i<n-1;i++){
-			MIN.push_back(v[i]);
-			MIN.push_back(v[n-i-1]);
-		}
-		sum=0;
-		for(int i=0;i<n-1;i++)
-			sum+=abs(MIN[i]-MIN[i+1]);
-		result=max(sum,result);
 	}else{
 		MAX.push_back(v[n/2]);
-		for(int i=0;i<n-1;i++){
-			MAX.push_back(v[n-i-1]);
-			MAX.push_back(v[i]);
-		}
-		sum=0;
-		for(int i=0;i<n-1;i++)
-			sum+=abs(MAX[i]-MAX[i+1]);
-		result=sum;
 		MIN.push_back(v[n/2]);
-		for(int i=0;i<n-1;i++){
-			MIN.push_back(v[i]);
-			MIN.push_back(v[n-i-1]);
-		}
-		sum=0;
-		for(int i=0;i<n-1;i++)
-			sum+=abs(MIN[i]-MIN[i+1]);	
-		result=max(sum,result);
 	}
-	cout<<result;
+	
+	for(int i=0;i<n-1;i++){
+		MAX.push_back(v[n-i-1]);
+		MAX.push_back(v[i]);
+	}
+	
+	for(int i=0;i<n-1;i++){
+		MIN.push_back(v[i]);
+		MIN.push_back(v[n-i-1]);
+	}
+	cout<<max(getSum(MIN),getSum(MAX));
 }
